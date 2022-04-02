0fb1edf1b7e88a0bd742e131e9bd469b3c356239
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-04-02 21:01:35 +0530

    init files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Landing from "./components/Landing";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
