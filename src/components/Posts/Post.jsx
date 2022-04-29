87e17b925b7b7e08c80ec08be37d7437b8ad9dfa
Nemesis <shuklaakshatascom@gmail.com>
2022-04-29 20:13:48 +0530

    Added Sticky Action Widget

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -8,6 +8,7 @@ import { DateTime } from "luxon";
 import Loader from "../../components/Utils/Loader";
 
 import Navbar from "../Navbar/Navbar";
+import StickyActionWidget from "./StickyActionWidget";
 
 const Post = () => {
   const { hash } = useParams();
@@ -20,6 +21,8 @@ const Post = () => {
   const [postDate, setPostDate] = useState(null);
   const [postCover, setPostCover] = useState(null);
   const [postBody, setPostBody] = useState(null);
+
+  const commentElement = useRef();
 
 
   useEffect(async () => {
@@ -51,59 +54,67 @@ const Post = () => {
     <>
       <Navbar title='Wurdne' />
 
-      <div className='mt-24 mx-auto w-screen md:w-5/6 px-2 md:px-8'>
+      <div className='mt-24 mx-auto md:w-5/6 px-2 md:px-8'>
         {isLoading ? (<Loader/>) : (
-        <div>
-          <div className='post-head'>
-            <div className='flex'>
-              <div className='p-2'>
-                <img
-                  src={`https://node.deso.org/api/v0/get-single-profile-picture/${posterKey}?fallback=https://node.deso.org/assets/img/default_profile_pic.png`}
-                  alt={`${postUsername}'s Avatar`}
-                  className='rounded-full h-12 w-12'
-                />
+          <div>
+            <div className='post-head'>
+              <div className='flex'>
+                <div className='p-2'>
+                  <img
+                    src={`https://node.deso.org/api/v0/get-single-profile-picture/${posterKey}?fallback=https://node.deso.org/assets/img/default_profile_pic.png`}
+                    alt={`${postUsername}'s Avatar`}
+                    className='rounded-full h-12 w-12'
+                  />
+                </div>
+                <div className='user-text flex flex-col justify-center p-2 text-gray-800'>
+                  <Link
+                    to={`/users/${postUsername}`}
+                    className='username font-bold hover:underline'>
+                    {postUsername}
+                  </Link>
+                  <div className='post-date text-sm'>{postDate}</div>
+                </div>
               </div>
-              <div className='user-text flex flex-col justify-center p-2 text-gray-800'>
-                <Link
-                  to={`/users/${postUsername}`}
-                  className='username font-bold hover:underline'>
-                  {postUsername}
-                </Link>
-                <div className='post-date text-sm'>{postDate}</div>
-              </div>
+              <div className='text-4xl font-bold break-words my-5 mx-3'>{postTitle}</div>
             </div>
-            <div className='text-4xl font-bold break-words my-5 mx-3'>{postTitle}</div>
+
+            <div className='cover-img py-4'>
+              <img
+                src={postCover}
+                alt=''
+                className='rounded-lg md:max-w-4xl w-full mx-auto'
+              />
+            </div>
+
+            <ReactMarkdown
+              className='post-content text-xl break-words unreset'
+              rehypePlugins={[rehypeHighlight]}
+              remarkPlugins={[remarkGfm]}>
+              {postBody}
+            </ReactMarkdown>
+
+            {/* 
+              <div className='tag-bar py-2 flex gap-2'>
+                {postTags.map((tag, idx) => {
+                  return (
+                    <Link
+                      key={idx}
+                      to={`/tag/${tag.replace(" ", "-")}`}
+                      className='tag-capsule p-2 border border-gray-600 rounded-full hover:bg-gray-600 hover:text-white duration-300'>
+                      # {tag}
+                    </Link>
+                  );
+                })}
+              </div> 
+            */}
+            
+            <div className="comments mt-4" ref={commentElement}>
+              <div className="title text-2xl font-bold separator">Comments</div>
+            </div>
+
+            <StickyActionWidget commentEl={commentElement} />
           </div>
-
-          <div className='cover-img py-4'>
-            <img
-              src={postCover}
-              alt=''
-              className='rounded-lg md:max-w-4xl w-full mx-auto'
-            />
-          </div>
-
-          <ReactMarkdown
-            className='post-content text-xl break-words unreset'
-            rehypePlugins={[rehypeHighlight]}
-            remarkPlugins={[remarkGfm]}>
-            {postBody}
-          </ReactMarkdown>
-
-      {/* 
-         <div className='tag-bar py-2 flex gap-2'>
-            {postTags.map((tag, idx) => {
-              return (
-                <Link
-                  key={idx}
-                  to={`/tag/${tag.replace(" ", "-")}`}
-                  className='tag-capsule p-2 border border-gray-600 rounded-full hover:bg-gray-600 hover:text-white duration-300'>
-                  # {tag}
-                </Link>
-              );
-            })}
-          </div> */}
-        </div> )}
+        )}
       </div>
     </>
   );
