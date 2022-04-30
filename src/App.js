e6b6d66d782ca083e36c662cc8f371a40840d1ef
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-04-30 12:28:36 +0530

    added temp user page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import Create from "./components/CreateBlog/Create";
 import Post from "./components/Posts/Post";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Landing />} />
         <Route path='/create' element={<Create />} />
         <Route path="/post/:hash" element={<Post />} />
+        <Route path="/u/:userName" element={<Profile/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Link, useParams } from "react-router-dom";
+import Deso from "deso-protocol";
+import Navbar from "../Navbar/Navbar";
+import Loader from "../../components/Utils/Loader";
+export default function Profile() {
+  const { userName } = useParams();
+  console.log(userName);
+  const [isLoading, setIsLoading] = useState(true);
+  const [profileData, setProfileData] = useState(null);
+  const deso = new Deso();
+  useEffect(async () => {
+    try {
+      const response = await deso.user.getSingleProfile({ Username: userName });
+      setProfileData(response);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+  return (
+    <>
+      <Navbar title='Wurdne' />
+      {isLoading ? (
+        <div className='my-28'>
+          {" "}
+          <Loader />
+        </div>
+      ) : profileData === null ? (
+        <div className='mx-auto md:w-5/6 px-2 py-12 md:px-8 my-32'>
+          profile not found with that username{" "}
+        </div>
+      ) : (
+        <div className='mx-auto md:w-5/6 px-2 py-32 md:px-8'>
+          <div className='flex flex-col md:flex-row'>
+            <div className='w-full md:w-1/2'>
+              <img
+                className='rounded-full w-32 h-32 md:w-48 md:h-48'
+                src={`https://diamondapp.com/api/v0/get-single-profile-picture/${profileData.Profile.PublicKeyBase58Check}?fallback=https://diamondapp.com/assets/img/default_profile_pic.png
+                    `}
+                alt='profile pic'
+              />
+            </div>
+            <div className='w-full md:w-1/2'>
+              <div className='flex flex-col md:flex-row'>
+                <div className='w-full md:w-1/2'>
+                  <h1 className='text-2xl font-bold'>
+                    {profileData.Profile.Username}
+                  </h1>
+                  <p className='text-gray-700'>
+                    {profileData.Profile.Description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
